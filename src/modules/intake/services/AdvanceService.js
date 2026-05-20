38b8efd78ae8d3eec75efef3d5c4df6734167859
muhammadbinmakbool <muhammadbinmakbool@gmail.com>
2026-05-20 16:57:25 +0500

    Fix Prisma Decimal RSC error in listAdvances

--- src/modules/intake/services/AdvanceService.js
+++ src/modules/intake/services/AdvanceService.js
@@ -6,7 +6,17 @@ export class AdvanceService {
     const advances = await AdvanceRepository.getAll();
     return advances.map(a => ({
       ...a,
-      amount: Number(a.amount)
+      amount: Number(a.amount),
+      intakeTransaction: a.intakeTransaction ? {
+        ...a.intakeTransaction,
+        grossWeight: Number(a.intakeTransaction.grossWeight),
+        netWeight: a.intakeTransaction.netWeight ? Number(a.intakeTransaction.netWeight) : null,
+        Bardana: a.intakeTransaction.Bardana ? Number(a.intakeTransaction.Bardana) : null,
+        Khot: a.intakeTransaction.Khot ? Number(a.intakeTransaction.Khot) : null,
+        normalizedWeight: Number(a.intakeTransaction.normalizedWeight),
+        rate: a.intakeTransaction.rate ? Number(a.intakeTransaction.rate) : null,
+        rateUnit: a.intakeTransaction.rateUnit || "KG"
+      } : null
     }));
   }
 
