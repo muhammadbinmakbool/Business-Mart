0156c29c7c9b47bb893a658576ca1c7dd8e6254b
muhammadbinmakbool <muhammadbinmakbool@gmail.com>
2026-07-06 14:48:38 +0500

    fix: align MSSQL schema and migrations to include missing isUserEditable columns

--- prisma/migrations_mssql/20260706093204_add_packaging_meta/migration.sql
+++ prisma/migrations_mssql/20260706093204_add_packaging_meta/migration.sql
@@ -8,6 +8,12 @@ ALTER TABLE [dbo].[IntakeTransaction] ADD [packagingMeta] NVARCHAR(max);
 -- AlterTable
 ALTER TABLE [dbo].[SaleItem] ADD [packagingMeta] NVARCHAR(max);
 
+-- AlterTable to add missing isUserEditable column
+ALTER TABLE [dbo].[TransactionAdjustment] ADD [isUserEditable] BIT NOT NULL CONSTRAINT [TransactionAdjustment_isUserEditable_df] DEFAULT 1;
+
+-- AlterTable to add missing isUserEditable column
+ALTER TABLE [dbo].[SupplierInvoiceAdjustment] ADD [isUserEditable] BIT NOT NULL CONSTRAINT [SupplierInvoiceAdjustment_isUserEditable_df] DEFAULT 1;
+
 COMMIT TRAN;
 
 END TRY
